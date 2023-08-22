d70fff8d74bd9ae46ecbbce6993513f5475cc647
s22004 <s22004@std.it-college.ac.jp>
2023-08-22 14:04:36 +0900

    データベースの最初だけ一旦

--- DatabaseSample/app/src/main/java/jp/ac/it_college/std/s22004/databasesample/MainActivity.kt
+++ DatabaseSample/app/src/main/java/jp/ac/it_college/std/s22004/databasesample/MainActivity.kt
@@ -9,13 +9,23 @@ import jp.ac.it_college.std.s22004.databasesample.databinding.ActivityMainBindin
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var _helper: DatabaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //DB helper を生成
+        _helper = DatabaseHelper(this)
+
+        // RecyclerView の初期化
         initList(binding.lvCocktail)
+    }
+
+    override fun onDestroy() {
+        _helper.close()      //　とても必要らしい
+        super.onDestroy()
     }
 
     private fun initList(list: RecyclerView) {
